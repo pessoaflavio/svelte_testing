d97a000e477e00af4d6c32b1b247ea56c842482a
Flavio Santanna <flaviosantanna@flavios-mbp.myfiosgateway.com>
2023-04-26 04:38:05 -0400

    testing svelte out

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,72 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    // console.log('hi')
+    import { onMount } from "svelte";
+
+    let artists = [];
+
+    onMount(async function () {
+        const response = await fetch('src/data/2023_amoeba2.json');
+        const data = await response.json();
+        console.log(data.artists);
+        artists = data.artists;
+    });
+    
+
+
+    // function handleMouseOver(event) {
+    //     o
+
+    // }
+    
+</script>
+<style>
+main {
+    margin: 0;
+    box-sizing: content-box;
+    font-family: 'IBM Plex Mono';
+    display: flex;
+    font-size: 10px;
+    flex-wrap: wrap;
+}
+.guest-container{
+    background-color: beige;
+    padding: 10px;
+    margin-bottom: 15px;
+    margin-right: 15px;
+    width: 300px;
+}
+h1{
+    font-size: 20px;
+}
+h2{
+    font-size: 12px;
+}
+.pick {
+    color: tomato;
+    /* width: 200px; */
+}
+</style>
+<h1>AMOEBA WHAT'S IN MY BAG</h1>
+<main>
+    
+  
+    {#each artists as artist}
+      <div class="guest-container">
+        <h2>{artist.guest}</h2>
+        <hr>
+        <p>
+            Picks: {artist.picks.length}
+        </p>
+        <hr>
+            {#each artist.picks as pick}
+                <p class="pick">
+                    
+                    W: {JSON.stringify(pick.album)} 
+                    A: {JSON.stringify(pick.artist)}
+                    M: {JSON.stringify(pick.medium)} 
+                </p>
+                <hr style="height:1px;border:none;background-color:#ffcc99">    
+            {/each} 
+      </div>
+    {/each}
+  </main>
